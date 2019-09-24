e9516098fb03c074bd5ce6225bd25da28776e7c7
AngusCornall <anguscornall@gmail.com>
2019-09-24 15:13:09 +1000

    Frame for management handling

--- js/manage.js
+++ js/manage.js
@@ -1,14 +1,32 @@
 
-let db = firebase.firestore();
+let db;
 
 function fetchClaimsStaff() {
     //TODO staff validation
+    db = firebase.firestore();
 
     let searchRoot = db.collection('users');
-
-    let fetchQuery = searchRoot.where('claims').get().then(function (querySnapshot) {
-
+    console.log("attempting fetch...");
+    searchRoot.get().then(function (querySnapshot) {
+        console.log("query fired..");
+        querySnapshot.forEach(function (doc) {
+            searchRoot.doc(doc.id).collection('claims').get().then(function (data) {
+                data.forEach(function (claim) {
+                    console.log(claim.id);
+                    searchRoot.doc(doc.id).collection('claims').doc(claim.id).get().then(function (claimData) {
+                        let claimObjPre = claimData.data();
+                        let claimObj = claimObjPre.claim;
+                        console.log(claimObj.additionalInfo);
+                        //console.log({claim : claim});
+                    })
+                })
+            });
+            console.log(doc.id);
+        });
     })
+        .catch(function (error) {
+            console.log("Fetch failed :(, => ", error);
+        });
 
     //for (){} //TODO iterate through retrieved query
 }
